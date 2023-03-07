ab9c55788678587f643bcde9b9143ebc153dc679
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-03-07 22:23:31 +0200

    Triangle area

--- TheTriangleFarmer/Program.cs
+++ TheTriangleFarmer/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("I am going to find an area of your triangle");
+Console.WriteLine("Please, enter a base of a triangle"); ;
+float triangle_base = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Please, enter a height of a triangle");
+float height = Convert.ToSingle(Console.ReadLine());
+float area = 0.5f * triangle_base * height;
+Console.WriteLine($"The area of your triangle is {area}");
+Console.ReadLine();
